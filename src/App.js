03aa751c9f45de4388334b8dac4b7da530855d39
EmmanuelOhore <emmanuelohore2003@gmail.com>
2024-11-11 20:32:25 +0100

    Initial commit for Quote Generator app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
+import "./styles/style.css";
 
 function App() {
+  useEffect(function quote() {
+    async function callquote() {
+      try {
+        const res = await fetch("https://dummyjson.com/quotes");
+        const data = await res.json();
+        setquote(data.quotes);
+        console.log(data.quotes);
+      } catch (error) {
+        console.log("eroor fecthing data:", error);
+      }
+    }
+    callquote();
+  }, []);
+  const [count, setcount] = useState(0);
+  const [author, setauthor] = useState("Mary Kay Ash");
+  const [quote, setquote] = useState("");
+  const [text, settest] = useState(
+    "Never give up beecause you never know if the next try is going to be the one that works"
+  );
+  const handlespeak = () => {
+    let word = text;
+    let value = new SpeechSynthesisUtterance(word);
+    window.speechSynthesis.speak(value);
+  };
+  const handelquote = () => {
+    let array = quote.map((item) => item);
+    let Qoute = array[count];
+    setcount((c) => c + 1);
+    settest(Qoute.quote);
+    setauthor(Qoute.author);
+  };
+  const handleCopy = () => {
+    navigator.clipboard
+      .writeText(text)
+      .then(() => {
+        toast.success("Text copied to clipboard");
+      })
+      .catch((err) => {
+        console.error("Error copying text: ", err);
+      });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ToastContainer />
+      <div className="container">
+        <div className="Header">
+          <h1>Quote of the Day</h1>
+        </div>
+        <div className="quote">
+          <p>{text}</p>
+          <div className="small">
+            {" "}
+            <small>---{author}</small>
+          </div>
+        </div>
+        <div className="footer">
+          <div className="icons">
+            <i onClick={handlespeak} class="fa-solid fa-volume-high"></i>
+            <i onClick={handleCopy} class="fa-regular fa-copy"></i>
+            <i class="fa-brands fa-github"></i>
+          </div>
+          <button onClick={handelquote}>New Quote</button>
+        </div>
+      </div>
+    </>
   );
 }
 
